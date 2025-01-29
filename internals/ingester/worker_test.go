ffbc9373e5e3dcad3dc7b248baa268367f255f1a
Paul Meyer <paul.meyer@mind7.fr>
2025-01-29 15:54:52 +0100

    Addes esapi package

--- internals/ingester/worker_test.go
+++ internals/ingester/worker_test.go
@@ -2,6 +2,7 @@ package ingester
 
 import (
 	"context"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
 	config "github.com/myrteametrics/myrtea-ingester-api/v5/internals/configuration"
 	"github.com/myrteametrics/myrtea-sdk/v5/connector"
 	"github.com/myrteametrics/myrtea-sdk/v5/elasticsearch"
